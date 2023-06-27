2891218191fd9398b4529ad5486b1cfa1378163c
Олег Пільнік <o.pilnik@municipal-guard.com.ua>
2023-06-27 22:05:15 +0300

    27_06_2023

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import DishCategory, Dish
+from .models import DishCategory, Dish, Event, Gallery
 
 
 admin.site.register(DishCategory)
 admin.site.register(Dish)
+admin.site.register(Event)
+admin.site.register(Gallery)
--- main_page/views.py
+++ main_page/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from .models import DishCategory
+from .models import DishCategory, Dish, Event, Gallery
 
 
 def main_page(request):
     categories = DishCategory.objects.filter(is_visible=True)
-    return render(request, 'index.html', context={'categories': categories})
+    dishes = Dish.objects.filter(is_visible=True, is_special=False)
+    special_dishes = Dish.objects.filter(is_special=True)
+    # events = Event.objects.all
+    # gallery = Gallery.objects.all
+
+
+    return render(request,
+                   "main.html",
+                    context={
+                        "categories": categories,
+                        "dishes": dishes,
+                        "special_dishes": special_dishes,
+                        })
